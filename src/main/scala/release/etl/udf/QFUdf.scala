b9af1c186b1fc8dec332c5c24b49f96415512f31
duyw <1650776224@qq.com>
2019-09-27 11:41:57 +0800

    updata 0927_1

---
+++ src/main/scala/release/etl/udf/QFUdf.scala
@@ -0,0 +1,24 @@
+package release.etl.udf
+
+import release.util.CommonUtil
+
+/**
+  * Spark UDF
+  */
+object QFUdf {
+
+  /**
+    * 年龄段
+    */
+  def getAgeRange(age:String):String={
+    var tseg = ""
+    try {
+      tseg = CommonUtil.getAgeRange(age)
+    }catch {
+      case ex:Exception=>{
+        println(s"$ex")
+      }
+    }
+    tseg
+  }
+}
